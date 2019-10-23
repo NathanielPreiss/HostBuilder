943cb0264037a3ee2f2d32bc0277993004955348
Nathaniel Preiss <npreiss@clubready.com>
2019-10-22 23:48:11 -0500

    Initial push

---
+++ HostBuilder/Program.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace HostBuilder
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = new Microsoft.Extensions.Hosting.HostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((context, configuration) =>
+                {
+                    configuration.AddJsonFile("appsettings.json", false, true);
+
+                    if (args != null)
+                        configuration.AddCommandLine(args);
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>((hostContext, builder) =>
+                {
+
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<IHostedService>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
